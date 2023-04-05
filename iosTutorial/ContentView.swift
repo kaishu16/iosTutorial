0ab3b07526be01613a00dd8b61f06880765713af
Kaishu Shimizu <kaishu16@KaishuMac.local>
2023-04-06 07:49:28 +0900

    initial commit

---
+++ iosTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iosTutorial
+//
+//  Created by Kaishu Shimizu on 2023/04/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Swift!")
+            .font(.largeTitle)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iosTutorial/iosTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosTutorialApp.swift
+//  iosTutorial
+//
+//  Created by Kaishu Shimizu on 2023/04/06.
+//
+
+import SwiftUI
+
+@main
+struct iosTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
